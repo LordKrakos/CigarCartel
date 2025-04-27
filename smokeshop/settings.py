a11b6fd7dd7f253a214c5150da806e305f6e8ea0
Joseph Krakos <Krakos.Joseph@gmail.com>
2025-04-27 14:27:39 -0400

    Prepare for Render deployment

--- smokeshop/settings.py
+++ smokeshop/settings.py
@@ -32,7 +32,11 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "419smokeshop.com",
+    "www.419smokeshop.com",
+    "storelocator-tt2a.onrender.com",
+]
 
 # Application definition
 
