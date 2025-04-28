ca66f8325cd25ac7cebb6bf6b4b3233d7ce9f783
Joseph Krakos <Krakos.Joseph@gmail.com>
2025-04-28 00:21:21 -0400

    Fix database connection issue

--- smokeshop/settings.py
+++ smokeshop/settings.py
@@ -91,11 +91,11 @@ WSGI_APPLICATION = 'smokeshop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT', default='5432'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
 
