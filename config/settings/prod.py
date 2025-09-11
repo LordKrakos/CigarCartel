91471bda301a4bfc67f52787abdc267e942ea244
Joseph Krakos <Krakos.Joseph@gmail.com>
2025-09-11 01:32:24 -0400

    updated

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -20,16 +20,4 @@ SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
-# Production DB
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT', default='5432'),
-    }
-}
-
 # Static & Media configs, Cloud storage configs, etc.
